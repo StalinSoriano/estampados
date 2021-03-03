e334067192f0a6885f54f9714dc3e745c2bf0025
unknown <david.2013681@gmail.com>
2021-03-02 23:23:42 -0500

    terminado parte informaiva 8

--- view/html/body.php
+++ view/html/body.php
@@ -61,7 +61,7 @@
                 </div>
 
             </div>
-            <div class="col-lg-3 ">
+            <div class="col-lg-3 "> 
 
                 <div class="card">
                     <img src="assets/img/a.jpg" alt="">
